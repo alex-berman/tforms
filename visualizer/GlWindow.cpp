dfa83f2ef28b4fb4cf345cf862e1520d28ca6b03
Alex Berman <alex@nimations.com>
2012-02-26 11:27:08 +0100

    added visualizer example

---
+++ visualizer/GlWindow.cpp
@@ -0,0 +1,79 @@
+// Copyright (C) 2011 Alexander Berman
+//
+// Sonotopy is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "GlWindow.hpp"
+#include <stdio.h>
+
+GlWindow *_glWindow;
+
+void GlWindow_display() {
+  _glWindow->glDisplay();
+}
+
+void GlWindow_reshape(int width, int height) {
+  _glWindow->glReshape(width, height);
+}
+
+void GlWindow_keyboard(unsigned char key, int x, int y) {
+  _glWindow->glKeyboard(key, x, y);
+}
+
+void GlWindow_special(int key, int x, int y) {
+  _glWindow->glSpecial(key, x, y);
+}
+
+GlWindow::GlWindow(int argc, char **argv, int _width, int _height) {
+  _glWindow = this;
+  windowWidth = _width;
+  windowHeight = _height;
+
+  glutInit(&argc, argv);
+  glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
+  glutInitWindowSize (windowWidth, windowHeight);
+  //glutInitWindowPosition (0, 0);
+  glutCreateWindow (argv[0]);
+  glutDisplayFunc(GlWindow_display);
+  glutIdleFunc(GlWindow_display);
+  glutReshapeFunc(GlWindow_reshape);
+  glutKeyboardFunc(GlWindow_keyboard);
+  glutSpecialFunc(GlWindow_special);
+}
+
+void GlWindow::glDisplay() {
+  display();
+}
+
+void GlWindow::glReshape(int _newWidth, int _newHeight) {
+  windowWidth = _newWidth;
+  windowHeight = _newHeight;
+  glViewport (0, 0, (GLint) windowWidth - 1, (GLint) windowHeight - 1);
+  glMatrixMode (GL_PROJECTION);
+  glLoadIdentity ();
+  glOrtho (0.0, (GLdouble) windowWidth, (GLdouble) windowHeight, 0.0, -1.0, 1.0);
+  resizedWindow();
+}
+
+void GlWindow::glText(int x, int y, const char *text) {
+  glPushMatrix();
+  glLoadIdentity();
+  glOrtho (0.0, (GLdouble) windowWidth, 0.0, (GLdouble) windowHeight, -1.0, 1.0);
+  glTranslatef(x, windowHeight - y, 0);
+  glLineWidth(1.0f);
+  glScalef(0.08f, 0.08f, 1.0f);
+
+  const char *i = text;
+  while (*i) glutStrokeCharacter(GLUT_STROKE_MONO_ROMAN, *i++);
+  glPopMatrix();
+}
---
+++ visualizer/Visualizer.cpp
@@ -0,0 +1,137 @@
+#include "GlWindow.hpp"
+#include "Stopwatch.hpp"
+#include <vector>
+#include <math.h>
+#include <lo/lo.h>
+#include <stdio.h>
+
+#define PORT "51234"
+
+using namespace std;
+
+float centerX, centerY;
+float screenRadius;
+
+class Chunk {
+public:
+  Chunk(float _angle, float _size, float _duration) {
+    angle = _angle;
+    duration = _duration;
+    size = _size;
+    t = 0;
+  }
+
+  void draw() {
+    float relativeTime = (float) t / duration;
+    float x1 = centerX + cos(angle) * (1-relativeTime) * (screenRadius+size);
+    float x2 = x1 + cos(angle) * size;
+    float y1 = centerY + sin(angle) * (1-relativeTime) * (screenRadius+size);
+    float y2 = y1 + sin(angle) * size;
+
+    glLineWidth(2);
+    glBegin(GL_LINES);
+    glColor3f(1, 1, 1);
+    glVertex2f(x1, y1);
+    glColor3f(0, 0, 0);
+    glVertex2f(x2, y2);
+    glEnd();
+  }
+
+  void update(float timeIncrement) {
+    t += timeIncrement;
+  }
+
+  bool arrived() {
+    return t >= duration;
+  }
+
+  float angle;
+  float duration;
+  float size;
+
+private:
+  float t;
+};
+
+class Visualizer : public GlWindow {
+public:
+  Visualizer(int argc, char **argv) : GlWindow(argc, argv, 500, 500) {
+    frameCount = 0;
+    glClearColor (0.0, 0.0, 0.0, 0.0);
+    glShadeModel (GL_SMOOTH);
+
+    centerX = windowWidth / 2;
+    centerY = windowHeight / 2;
+    screenRadius = windowWidth / 2;
+  }
+
+  void display() {
+    if(frameCount == 0) {
+      stopwatch.start();
+      timeIncrement = 0;
+    }
+    else {
+      float timeOfThisDisplay = stopwatch.getElapsedMilliseconds();
+      timeIncrement = timeOfThisDisplay - timeOfPreviousDisplay;
+      timeOfPreviousDisplay = timeOfThisDisplay;
+    }
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    vector< vector<Chunk>::iterator > chunksToDelete;
+    for(vector<Chunk>::iterator i = chunks.begin(); i != chunks.end(); i++) {
+      i->draw();
+      i->update(timeIncrement);
+      if(i->arrived())
+	chunksToDelete.push_back(i);
+    }
+
+    for(vector< vector<Chunk>::iterator >::reverse_iterator i = chunksToDelete.rbegin();
+	i != chunksToDelete.rend(); i++) {
+      chunks.erase(*i);
+    }
+
+    glutSwapBuffers();
+    frameCount++;
+  }
+
+  void addChunk(float angle, float size, float durationSecs) {
+    float duration = durationSecs * 1000;
+    Chunk chunk(angle, size, duration);
+    chunks.push_back(chunk);
+  }
+
+private:
+  vector<Chunk> chunks;
+  Stopwatch stopwatch;
+  unsigned long frameCount;
+  float timeOfPreviousDisplay;
+  float timeIncrement;
+};
+
+
+Visualizer *visualizer;
+
+int chunkHandler(const char *path, const char *types,
+		 lo_arg **argv, int argc,
+		 void *data, void *user_data) {
+  int size = argv[0]->i;
+  float duration = argv[1]->f;
+  float angle = argv[2]->f;
+  visualizer->addChunk(angle, size/50, duration);
+  return 0;
+}
+
+void error(int num, const char *msg, const char *path) {
+  printf("liblo server error %d in path %s: %s\n", num, path, msg);
+}
+
+int main(int argc, char **argv) {
+  visualizer = new Visualizer(argc, argv);
+
+  lo_server_thread st = lo_server_thread_new(PORT, error);
+  lo_server_thread_add_method(st, "/chunk", "iff", chunkHandler, NULL);
+  lo_server_thread_start(st);
+
+  glutMainLoop();
+}
