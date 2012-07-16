87e1c42d0f8c90bcf15d7a732bb295d492445165
Alex Berman <alex@nimations.com>
2012-07-16 12:23:49 +0200

    towards visualization in python

--- visual-experiments/simple.py
+++ visual-experiments/simple.py
@@ -8,88 +8,84 @@ from orchestra import VISUALIZER_PORT
 
 ESCAPE = '\033'
 
-def handle_chunk(path, args, types, src, data):
-    print "received message '%s'" % path
+class Visualizer:
+    def __init__(self):
+        self.setup_osc()
+        glutInit(sys.argv)
+        glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
+        glutInitWindowSize(640, 480)
+        glutInitWindowPosition(0, 0)
+        glutCreateWindow("")
+        glutDisplayFunc(self.DrawGLScene)
+        glutIdleFunc(self.DrawGLScene)
+        glutReshapeFunc(self.ReSizeGLScene)
+        glutKeyboardFunc(self.keyPressed)
+        self.InitGL(640, 480)
+        glutMainLoop()
 
-def setup_osc():
-    global server
-    server = liblo.Server(VISUALIZER_PORT)
-    server.add_method("/chunk", "ifff", handle_chunk)
+    def handle_chunk(self, path, args, types, src, data):
+        print "received message '%s'" % path
 
-    
-def InitGL(Width, Height):
-    glClearColor(0.0, 0.0, 0.0, 0.0)
-    glClearDepth(1.0)
-    glDepthFunc(GL_LESS)
-    glEnable(GL_DEPTH_TEST)
-    glShadeModel(GL_SMOOTH)
-	
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
+    def setup_osc(self):
+        self.server = liblo.Server(VISUALIZER_PORT)
+        self.server.add_method("/chunk", "ifff", self.handle_chunk)
 
-    glMatrixMode(GL_MODELVIEW)
+    def InitGL(self, Width, Height):
+        glClearColor(0.0, 0.0, 0.0, 0.0)
+        glClearDepth(1.0)
+        glDepthFunc(GL_LESS)
+        glEnable(GL_DEPTH_TEST)
+        glShadeModel(GL_SMOOTH)
 
-def ReSizeGLScene(Width, Height):
-    if Height == 0:
-	    Height = 1
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
 
-    glViewport(0, 0, Width, Height)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
-    glMatrixMode(GL_MODELVIEW)
+        glMatrixMode(GL_MODELVIEW)
 
-def DrawGLScene():
-    global server
-    server.recv(10)
+    def ReSizeGLScene(self, Width, Height):
+        if Height == 0:
+                Height = 1
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glLoadIdentity()					# Reset The View 
+        glViewport(0, 0, Width, Height)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
+        glMatrixMode(GL_MODELVIEW)
 
-    # Move Left 1.5 units and into the screen 6.0 units.
-    glTranslatef(-1.5, 0.0, -6.0)
+    def DrawGLScene(self):
+        self.server.recv(10)
 
-    # Draw a triangle
-    glBegin(GL_POLYGON)                 # Start drawing a polygon
-    glVertex3f(0.0, 1.0, 0.0)           # Top
-    glVertex3f(1.0, -1.0, 0.0)          # Bottom Right
-    glVertex3f(-1.0, -1.0, 0.0)         # Bottom Left
-    glEnd()                             # We are done with the polygon
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glLoadIdentity()					# Reset The View 
+
+        # Move Left 1.5 units and into the screen 6.0 units.
+        glTranslatef(-1.5, 0.0, -6.0)
+
+        # Draw a triangle
+        glBegin(GL_POLYGON)                 # Start drawing a polygon
+        glVertex3f(0.0, 1.0, 0.0)           # Top
+        glVertex3f(1.0, -1.0, 0.0)          # Bottom Right
+        glVertex3f(-1.0, -1.0, 0.0)         # Bottom Left
+        glEnd()                             # We are done with the polygon
 
 
-    # Move Right 3.0 units.
-    glTranslatef(3.0, 0.0, 0.0)
+        # Move Right 3.0 units.
+        glTranslatef(3.0, 0.0, 0.0)
 
-    # Draw a square (quadrilateral)
-    glBegin(GL_QUADS)                   # Start drawing a 4 sided polygon
-    glVertex3f(-1.0, 1.0, 0.0)          # Top Left
-    glVertex3f(1.0, 1.0, 0.0)           # Top Right
-    glVertex3f(1.0, -1.0, 0.0)          # Bottom Right
-    glVertex3f(-1.0, -1.0, 0.0)         # Bottom Left
-    glEnd()                             # We are done with the polygon
+        # Draw a square (quadrilateral)
+        glBegin(GL_QUADS)                   # Start drawing a 4 sided polygon
+        glVertex3f(-1.0, 1.0, 0.0)          # Top Left
+        glVertex3f(1.0, 1.0, 0.0)           # Top Right
+        glVertex3f(1.0, -1.0, 0.0)          # Bottom Right
+        glVertex3f(-1.0, -1.0, 0.0)         # Bottom Left
+        glEnd()                             # We are done with the polygon
 
-    glutSwapBuffers()
+        glutSwapBuffers()
 
-def keyPressed(*args):
-    if args[0] == ESCAPE:
-	    sys.exit()
-
-def main():
-	global window
-        setup_osc()
-	glutInit(sys.argv)
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
-	glutInitWindowSize(640, 480)
-	glutInitWindowPosition(0, 0)
-	window = glutCreateWindow("")
-	glutDisplayFunc(DrawGLScene)
-	glutIdleFunc(DrawGLScene)
-	glutReshapeFunc(ReSizeGLScene)
-	glutKeyboardFunc(keyPressed)
-	InitGL(640, 480)
-	glutMainLoop()
+    def keyPressed(self, *args):
+        if args[0] == ESCAPE:
+                sys.exit()
 
 print "Hit ESC key to quit."
-main()
-    	
+Visualizer()
