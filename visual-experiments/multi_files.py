33455aca1d58ca93583a0c837a497ee37b4ee276
Alex Berman <alex@nimations.com>
2012-07-17 11:08:16 +0200

    file-specific scaling

--- visual-experiments/multi_files.py
+++ visual-experiments/multi_files.py
@@ -36,12 +36,37 @@ class Chunk:
         self.pan = pan
         self.arrival_time = arrival_time
 
-class Visualizer:
-    def __init__(self, width=640, height=480):
-        self.chunks = []
-        self.x_ratio = None
+class File:
+    def __init__(self, filenum):
+        self.filenum = filenum
         self._smoothed_min_byte = Smoother()
         self._smoothed_max_byte = Smoother()
+        self.x_ratio = None
+        self.chunks = []
+
+    def add_chunk(self, chunk):
+        self.chunks.append(chunk)
+        self.update_x_scope()
+
+    def update_x_scope(self):
+        min_byte = min(self.chunks, key=lambda chunk: chunk.begin).begin
+        max_byte = max(self.chunks, key=lambda chunk: chunk.end).end
+        self._smoothed_min_byte.smooth(min_byte)
+        self._smoothed_max_byte.smooth(max_byte)
+        self.byte_offset = self._smoothed_min_byte.value()
+        diff = self._smoothed_max_byte.value() - self._smoothed_min_byte.value()
+        if diff == 0:
+            self.x_ratio = 1
+        else:
+            self.x_ratio = 1.0 / diff
+
+    def byte_to_coord(self, byte):
+        return self.x_ratio * (byte - self.byte_offset)
+
+class Visualizer:
+    def __init__(self, width=640, height=480):
+        self.files = {}
+        self.chunks = []
         self._smoothed_min_filenum = Smoother()
         self._smoothed_max_filenum = Smoother()
 
@@ -62,6 +87,9 @@ class Visualizer:
     def handle_chunk(self, path, args, types, src, data):
         (torrent_position, byte_size, filenum, file_offset, duration, pan) = args
         chunk = Chunk(torrent_position, byte_size, filenum, file_offset, pan, time.time())
+        if not filenum in self.files:
+            self.files[filenum] = File(filenum)
+        self.files[filenum].add_chunk(chunk)
         self.chunks.append(chunk)
 
     def setup_osc(self):
@@ -100,54 +128,36 @@ class Visualizer:
 
 
     def draw_chunks(self):
-        self.update_scope()
-        for chunk in self.chunks:
-            self.draw_chunk(chunk)
+        self.update_y_scope()
+        for f in self.files.values():
+            self.draw_file(f)
 
-    def draw_chunk(self, chunk):
-        age = time.time() - chunk.arrival_time
-        if age > HIGHLIGHT_TIME:
-            actuality = 0
-        else:
-            actuality = 1 - float(age) / HIGHLIGHT_TIME
-        pan_x = (chunk.pan - 0.5)
-        y = self.filenum_to_y_coord(chunk.filenum)
-        y1 = int(y - 3 + 20 * pan_x * actuality)
-        y2 = int(y + 3 + 20 * pan_x * actuality)
-        x1 = int(self.byte_to_x_coord(chunk.begin))
-        x2 = int(self.byte_to_x_coord(chunk.end))
-        if x2 == x1:
-            x2 = x1 + 1
-        opacity = 0.5 + actuality / 2
-        glColor3f(opacity, opacity, opacity)
-        glBegin(GL_LINE_STRIP)
-        glVertex2i(x1, y2)
-        glVertex2i(x2, y2)
-        glVertex2i(x2, y1)
-        glVertex2i(x1, y1)
-        glEnd()
-
-    def byte_to_x_coord(self, byte):
-        return self.x_ratio * (byte - self.byte_offset)
+    def draw_file(self, f):
+        y = self.filenum_to_y_coord(f.filenum)
+        for chunk in f.chunks:
+            age = time.time() - chunk.arrival_time
+            if age > HIGHLIGHT_TIME:
+                actuality = 0
+            else:
+                actuality = 1 - float(age) / HIGHLIGHT_TIME
+            pan_x = (chunk.pan - 0.5)
+            y1 = int(y - 3 + 40 * pan_x * actuality)
+            y2 = int(y + 3 + 40 * pan_x * actuality)
+            x1 = int(f.byte_to_coord(chunk.begin) * self.width)
+            x2 = int(f.byte_to_coord(chunk.end) * self.width)
+            if x2 == x1:
+                x2 = x1 + 1
+            opacity = 0.2 + (actuality * 0.8)
+            glColor3f(opacity, opacity, opacity)
+            glBegin(GL_LINE_STRIP)
+            glVertex2i(x1, y2)
+            glVertex2i(x2, y2)
+            glVertex2i(x2, y1)
+            glVertex2i(x1, y1)
+            glEnd()
 
     def filenum_to_y_coord(self, filenum):
         return self.y_ratio * (filenum - self.filenum_offset + 1)
-
-    def update_scope(self):
-        self.update_x_scope()
-        self.update_y_scope()
-
-    def update_x_scope(self):
-        min_byte = min(self.chunks, key=lambda chunk: chunk.begin).begin
-        max_byte = max(self.chunks, key=lambda chunk: chunk.end).end
-        self._smoothed_min_byte.smooth(min_byte)
-        self._smoothed_max_byte.smooth(max_byte)
-        self.byte_offset = self._smoothed_min_byte.value()
-        diff = self._smoothed_max_byte.value() - self._smoothed_min_byte.value()
-        if diff == 0:
-            self.x_ratio = 1
-        else:
-            self.x_ratio = float(self.width) / diff
 
     def update_y_scope(self):
         min_filenum = min(self.chunks, key=lambda chunk: chunk.filenum).filenum
