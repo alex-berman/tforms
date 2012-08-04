78b7d16cd99ae1cc5ae5b2b2727764115ed0532a
Alex Berman <alex@nimations.com>
2012-08-04 10:41:02 +0200

    encode script truncates audio stream if longer than desired duration

--- visual-experiments/encode.sh
+++ visual-experiments/encode.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sox capture-mastered.wav capture-trimmed.wav trim $1
 rm capture.mp4
-glc-play capture.glc -o - -y 1 | ffmpeg -i - -i capture-trimmed.wav -acodec libmp3lame -ab 128k -ac 2 -vcodec libx264 -vpre slow -ss $2 -t $3 capture.mp4
+glc-play capture.glc -o - -y 1 | ffmpeg  -t $3 -i - -i capture-trimmed.wav -acodec libmp3lame -ab 128k -ac 2 -vcodec libx264 -vpre slow -ss $2 -t $3 capture.mp4
 rm capture-trimmed.wav
