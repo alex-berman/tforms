b82e887bea7a6025bfb7ac8e57fd1baa8bfc37ad
Alex Berman <alex@nimations.com>
2012-08-12 10:08:50 +0200

    more stable synth controller

--- synth_controller.py
+++ synth_controller.py
@@ -4,45 +4,65 @@ import time
 
 PRECISION = .01
 
+class SynthControllerException(Exception):
+    pass
+
 class Player:
-    def __init__(self, synth, player_id):
+    def __init__(self, synth, id):
         self.synth = synth
-        self.player_id = player_id
-
-    def start_playing(self, sound_id, position, pan):
-        return Sound(self.synth, self.player_id, sound_id, position, pan)
-
-class Sound:
-    def __init__(self, synth, player_id, sound_id, position, pan):
-        self.synth = synth
-        self.player_id = player_id
-        self.synth._send("/start", player_id, sound_id, position, pan)
-        self.cursor = position
-
-    def stop_playing(self):
-        self.synth._send("/stop", self.player_id)
-
-    def play_to(self, target_position, desired_duration):
-        thread = threading.Thread(target=self._play_to,
-                                  args=(target_position, desired_duration))
+        self.id = id
+        self._target_position = None
+        self._cursor = None
+        self._desired_duration = None
+        thread = threading.Thread(target=self._cursor_thread)
         thread.daemon = True
         thread.start()
 
-    def _play_to(self, target_position, desired_duration):
-        self._start_time = time.time()
-        start_position = self.cursor
-        distance = target_position - start_position
-        while self.elapsed_time() < desired_duration:
-            position = start_position + self.elapsed_time() / desired_duration * distance
-            self.set_cursor(position)
-            time.sleep(PRECISION)
+    def start_playing(self, sound_id, position, pan):
+        if self._desired_duration:
+            raise SynthControllerException("trying to start new sound while already playing something")
+        self.synth._send("/start", self.id, sound_id, position, pan)
+        self._cursor = position
+        return Sound(self)
 
-    def elapsed_time(self):
+    def stop_playing(self):
+        self.synth._send("/stop", self.id)
+        self._desired_duration = None
+
+    def play_to(self, target_position, desired_duration):
+        self._target_position = target_position
+        self._start_time = time.time()
+        self._start_position = self._cursor
+        self._distance = target_position - self._start_position
+        self._desired_duration = desired_duration
+
+    def _cursor_thread(self):
+        while True:
+            while self._desired_duration == None:
+                time.sleep(PRECISION)
+            while self._elapsed_time() < self._desired_duration:
+                position = self._start_position + \
+                    self._elapsed_time() / self._desired_duration * self._distance
+                self.set_cursor(position)
+                time.sleep(PRECISION)
+            self._desired_duration = None
+
+    def _elapsed_time(self):
         return time.time() - self._start_time
 
     def set_cursor(self, position):
-        self.synth._send("/cursor", self.player_id, position)
-        self.cursor = position
+        self.synth._send("/cursor", self.id, position)
+        self._cursor = position
+
+class Sound:
+    def __init__(self, player):
+        self.player = player
+
+    def stop_playing(self):
+        self.player.stop_playing()
+
+    def play_to(self, target_position, desired_duration):
+        self.player.play_to(target_position, desired_duration)
 
 class SynthController:
     PORT = 57120
@@ -51,12 +71,19 @@ class SynthController:
         self.target = liblo.Address(self.PORT)
         self._lock = threading.Lock()
         self._player_count = 1
+        self._players = []
 
     def load_sound(self, sound_id, filename):
         self._send("/load", sound_id, filename)
 
     def player(self):
-        return Player(self, self._new_player_id())
+        p = Player(self, self._new_player_id())
+        self._players.append(p)
+        return p
+
+    def stop_all_players(self):
+        for player in self._players:
+            player.stop_playing()
 
     def _new_player_id(self):
         id = self._player_count
--- test_synth_controller.py
+++ test_synth_controller.py
@@ -1,12 +1,36 @@
-from synth_controller import SynthController
+from synth_controller import SynthController, SynthControllerException
 import time
+import unittest
 
 synth = SynthController()
 synth.load_sound(sound_id=1, filename="sonic-experiments/theme-nikolayeva-mono.wav")
-player = synth.player()
-sound = player.start_playing(sound_id=1, position=0, pan=0.5)
-sound.play_to(target_position=0.5, desired_duration=2.0)
-time.sleep(2.0)
-sound.play_to(target_position=1.0, desired_duration=1.0)
-time.sleep(1.0)
-sound.stop_playing()
+synth.load_sound(sound_id=2, filename="sonic-experiments/theme-acad-st-martin-mono.wav")
+
+class SynthControllerTest(unittest.TestCase):
+    def test_play_sound_with_different_speeds(self):
+        player = synth.player()
+        sound = player.start_playing(sound_id=1, position=0, pan=0.5)
+        sound.play_to(target_position=0.5, desired_duration=2.0)
+        time.sleep(2.0)
+        sound.play_to(target_position=1.0, desired_duration=1.0)
+        time.sleep(1.0)
+        sound.stop_playing()
+
+    def test_trying_to_play_two_sounds_simultaneously_with_same_player_raises_exception(self):
+        player = synth.player()
+        sound1 = player.start_playing(sound_id=1, position=0, pan=0.5)
+        sound1.play_to(target_position=0.5, desired_duration=2.0)
+        with self.assertRaises(SynthControllerException):
+            sound2 = player.start_playing(sound_id=2, position=0, pan=0.5)
+
+    def test_two_players(self):
+        player1 = synth.player()
+        sound1 = player1.start_playing(sound_id=1, position=0, pan=0)
+        sound1.play_to(target_position=1.0, desired_duration=3.0)
+        time.sleep(1.5)
+        player2 = synth.player()
+        sound2 = player2.start_playing(sound_id=2, position=0, pan=1)
+        sound2.play_to(target_position=1.0, desired_duration=3.0)
+        time.sleep(3.0)
+        sound1.stop_playing()
+        sound2.stop_playing()
