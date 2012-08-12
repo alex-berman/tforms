d95436de365dac5ab7f5b07cb6a878309a8b3054
Alex Berman <alex@nimations.com>
2012-08-12 08:34:16 +0200

    refactoring

--- synth_controller.py
+++ synth_controller.py
@@ -1,6 +1,20 @@
 import liblo
 import threading
 
+class Sound:
+    def __init__(self, synth, player_id, sound_id, position, pan):
+        self.synth = synth
+        self.player_id = player_id
+        self.synth._send("/start", player_id, sound_id, position, pan)
+        self.cursor = position
+
+    def stop_playing(self):
+        self.synth._send("/stop", self.player_id)
+
+    def set_cursor(self, position):
+        self.synth._send("/cursor", self.player_id, position)
+        self.cursor = position
+
 class SynthController:
     PORT = 57120
 
@@ -12,13 +26,7 @@ class SynthController:
         self._send("/load", sound_id, filename)
 
     def start_playing(self, player_id, sound_id, position, pan):
-        self._send("/start", player_id, sound_id, position, pan)
-
-    def stop_playing(self, player_id):
-        self._send("/stop", player_id)
-
-    def set_cursor(self, player_id, position):
-        self._send("/cursor", player_id, position)
+        return Sound(self, player_id, sound_id, position, pan)
 
     def sync_beep(self):
         self._send("/sync_beep")
--- test_synth.py
+++ test_synth.py
@@ -2,16 +2,16 @@ from synth_controller import SynthController
 import time
 
 DURATION = 13.83
-s = SynthController()
-s.load_sound(sound_id=1, filename="sonic-experiments/theme-nikolayeva-mono.wav")
-s.start_playing(player_id=1, sound_id=1, position=0, pan=0.5)
+synth = SynthController()
+synth.load_sound(sound_id=1, filename="sonic-experiments/theme-nikolayeva-mono.wav")
+sound = synth.start_playing(player_id=1, sound_id=1, position=0, pan=0.1)
 
 start_time=time.time()
 position = 0
 while position < 1:
     position = (time.time() - start_time) / DURATION
     position = pow(position, .5)
-    s.set_cursor(player_id=1, position=position)
+    sound.set_cursor(position)
     time.sleep(.01)
 
-s.stop_playing(player_id=1)
+sound.stop_playing()
