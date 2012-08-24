52e3e6f56cd1519bcde73cd541b94b485a351a5d
Alex Berman <alex@nimations.com>
2012-08-24 11:01:51 +0200

    progress plot seems to work

--- plot_progress.py
+++ plot_progress.py
@@ -5,6 +5,9 @@ from argparse import ArgumentParser
 import subprocess
 import matplotlib
 import colors
+sys.path.insert(0, "visual-experiments")
+from gatherer import Gatherer
+from visualizer import Chunk
 
 parser = ArgumentParser()
 parser.add_argument("sessiondir", type=str)
@@ -13,6 +16,7 @@ parser.add_argument("-n", "--filenum", dest="filenum", type=int)
 parser.add_argument("-width", type=float, default=500)
 parser.add_argument("-height", type=float, default=500)
 parser.add_argument("-stroke_width", type=float, default=5)
+parser.add_argument("-resolution", type=float, help="Horizontal dots per second", default=100.0)
 options = parser.parse_args()
 
 logfilename = "%s/session.log" % options.sessiondir
@@ -26,15 +30,35 @@ print '<svg xmlns="http://www.w3.org/2000/svg" version="1.1">'
 def time_to_x(t): return t / log.lastchunktime() * options.width
 def byte_to_y(byte_pos): return float(byte_pos) / total_size * options.height
 
-peer_colors = colors.colors(len(log.peers))
+class Piece:
+    def __init__(self, begin, end):
+        self.begin = begin
+        self.end = end
 
-files = {}
+peer_colors = colors.colors(len(log.peers))
+peers = {}
+
+def render_slice(t):
+    x1 = x2 = time_to_x(t)
+    for gatherer, color in zip(peers.values(), peer_colors):
+        for piece in gatherer.pieces():
+            y1 = byte_to_y(piece.begin)
+            y2 = byte_to_y(piece.end)
+            (r,g,b,a) = color
+            print '  <line x1="%f" y1="%f" x2="%f" y2="%f" stroke="rgb(%i,%i,%i)" stroke-width="%f" />' % (
+                x1, y1, x2, y2, r*255, g*255, b*255, options.stroke_width)
+
+previous_slice_time = None
+seconds_per_slice = 1.0 / options.resolution
 for chunk in log.chunks:
-    x1 = x2 = time_to_x(chunk["t"])
-    y1 = byte_to_y(chunk["begin"])
-    y2 = byte_to_y(chunk["end"])
-    (r,g,b,a) = peer_colors[log.peeraddr_to_id[chunk["peeraddr"]]]
-    print '  <line x1="%f" y1="%f" x2="%f" y2="%f" stroke="rgb(%i,%i,%i)" stroke-width="%f" />' % (
-        x1, y1, x2, y2, r*255, g*255, b*255, options.stroke_width)
+    peer_id = log.peeraddr_to_id[chunk["peeraddr"]]
+    try:
+        peer = peers[peer_id]
+    except KeyError:
+        peer = peers[peer_id] = Gatherer()
+    peer.add(Piece(chunk["begin"], chunk["end"]))
+    if previous_slice_time is None or (chunk["t"] - previous_slice_time) >= seconds_per_slice:
+        render_slice(chunk["t"])
+        previous_slice_time = chunk["t"]
 
 print '</svg>'
