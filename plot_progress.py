14f0d38345cf9322ae08d84f78729f5cc263fa63
Alex Berman <alex@nimations.com>
2012-08-24 11:39:08 +0200

    progress plot optimized by using rasterized image instead of SVG

--- plot_progress.py
+++ plot_progress.py
@@ -5,6 +5,7 @@ from argparse import ArgumentParser
 import subprocess
 import matplotlib
 import colors
+from PIL import Image
 sys.path.insert(0, "visual-experiments")
 from gatherer import Gatherer
 from visualizer import Chunk
@@ -13,10 +14,10 @@ parser = ArgumentParser()
 parser.add_argument("sessiondir", type=str)
 parser.add_argument("-t", "--torrent", dest="torrentname", default="")
 parser.add_argument("-n", "--filenum", dest="filenum", type=int)
-parser.add_argument("-width", type=float, default=500)
-parser.add_argument("-height", type=float, default=500)
+parser.add_argument("-width", type=int, default=500)
+parser.add_argument("-height", type=int, default=500)
 parser.add_argument("-stroke_width", type=float, default=5)
-parser.add_argument("-resolution", type=float, help="Horizontal dots per second", default=100.0)
+parser.add_argument("-output", default="progress.png")
 options = parser.parse_args()
 
 logfilename = "%s/session.log" % options.sessiondir
@@ -25,31 +26,31 @@ print >> sys.stderr, "found %d chunks" % len(log.chunks)
 
 total_size = max([chunk["end"] for chunk in log.chunks])
 
-print '<svg xmlns="http://www.w3.org/2000/svg" version="1.1">'
-
-def time_to_x(t): return t / log.lastchunktime() * options.width
-def byte_to_y(byte_pos): return float(byte_pos) / total_size * options.height
+def byte_to_y(byte_pos): return min(int(float(byte_pos) / total_size * options.height), options.height-1)
 
 class Piece:
     def __init__(self, begin, end):
         self.begin = begin
         self.end = end
 
-peer_colors = colors.colors(len(log.peers))
+peer_colors = [(int(r*255), int(g*255), int(b*255))
+                for (r,g,b,a) in colors.colors(len(log.peers))]
 peers = {}
 
-def render_slice(t):
-    x1 = x2 = time_to_x(t)
-    for gatherer, color in zip(peers.values(), peer_colors):
+image = Image.new("RGB", (options.width, options.height), "white")
+pixels = image.load()
+
+def render_slice(x):
+    for (gatherer, color) in zip(peers.values(), peer_colors):
         for piece in gatherer.pieces():
             y1 = byte_to_y(piece.begin)
             y2 = byte_to_y(piece.end)
-            (r,g,b,a) = color
-            print '  <line x1="%f" y1="%f" x2="%f" y2="%f" stroke="rgb(%i,%i,%i)" stroke-width="%f" />' % (
-                x1, y1, x2, y2, r*255, g*255, b*255, options.stroke_width)
+            for y in range(y1, y2):
+                pixels[x, y] = color
 
 previous_slice_time = None
-seconds_per_slice = 1.0 / options.resolution
+seconds_per_slice = log.lastchunktime() / options.width
+x = 0
 for chunk in log.chunks:
     peer_id = log.peeraddr_to_id[chunk["peeraddr"]]
     try:
@@ -58,7 +59,10 @@ for chunk in log.chunks:
         peer = peers[peer_id] = Gatherer()
     peer.add(Piece(chunk["begin"], chunk["end"]))
     if previous_slice_time is None or (chunk["t"] - previous_slice_time) >= seconds_per_slice:
-        render_slice(chunk["t"])
+        render_slice(x)
+        x += 1
+        if x == options.width:
+            break
         previous_slice_time = chunk["t"]
 
-print '</svg>'
+image.save(options.output)
