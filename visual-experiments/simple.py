1a8a5bb5cd1f3eb3b62809a6506b022912e5a26f
Alex Berman <alex@nimations.com>
2012-09-03 09:13:56 +0200

    simple vis: clearer playback via extended height

--- visual-experiments/simple.py
+++ visual-experiments/simple.py
@@ -6,12 +6,13 @@ from collections import OrderedDict
 from vector import Vector3d
 
 MARGIN = 20
-HEIGHT = 10
+GATHERED_HEIGHT = 10
+PLAYING_HEIGHT = 15
 BACKGROUND_COLOR = Vector3d(.9, .9, .9)
 GATHERED_COLOR = Vector3d(.7, .9, .7)
 PLAYING_COLOR = Vector3d(1, 0, 0)
-FADE_IN = 0.005
-FADE_OUT = 0.006
+FADE_IN = 0.05
+FADE_OUT = 0.06
 
 class Smoother:
     RESPONSE_FACTOR = 5
@@ -48,16 +49,16 @@ class File(visualizer.File):
             del self.playing_segments[segment_id]
 
     def render(self):
-        y = float(self.visualizer.height) / (self.visualizer.num_files + 1) * (self.filenum + 1)
-        self.y1 = int(y - HEIGHT/2)
-        self.y2 = int(y + HEIGHT/2)
+        self.y = float(self.visualizer.height) / (self.visualizer.num_files + 1) * (
+            self.filenum + 1)
+        self.y1 = int(self.y - GATHERED_HEIGHT/2)
+        self.y2 = int(self.y + GATHERED_HEIGHT/2)
 
         self.draw_background()
         self.draw_gathered_segments()
         self.draw_playing_segments()
 
     def draw_background(self):
-        height = 3
         x1 = self.byte_to_px(0)
         x2 = self.byte_to_px(self.length)
         self.visualizer.set_color(BACKGROUND_COLOR)
@@ -87,15 +88,18 @@ class File(visualizer.File):
         glEnd()
 
     def draw_playing_segment(self, segment):
+        height = self.playing_height(segment)
+        y1 = int(self.y - height/2)
+        y2 = int(self.y + height/2)
         self.visualizer.set_color(self.playing_color(segment))
         x1 = self.byte_to_px(segment.begin)
         x2 = self.byte_to_px(segment.begin + segment.byte_size * segment.relative_age())
         x2 = max(x2, x1 + 1)
         glBegin(GL_QUADS)
-        glVertex2i(x1, self.y2)
-        glVertex2i(x2, self.y2)
-        glVertex2i(x2, self.y1)
-        glVertex2i(x1, self.y1)
+        glVertex2i(x1, y2)
+        glVertex2i(x2, y2)
+        glVertex2i(x2, y1)
+        glVertex2i(x1, y1)
         glEnd()
 
     def playing_color(self, segment):
@@ -107,6 +111,16 @@ class File(visualizer.File):
                 (1 - segment.relative_age()) / FADE_OUT)
         else:
             return PLAYING_COLOR
+
+    def playing_height(self, segment):
+        if segment.relative_age() < FADE_IN:
+            return GATHERED_HEIGHT + (PLAYING_HEIGHT - GATHERED_HEIGHT) * (
+                segment.relative_age() / FADE_IN)
+        elif (1 - segment.relative_age() < FADE_OUT):
+            return GATHERED_HEIGHT + (PLAYING_HEIGHT - GATHERED_HEIGHT) * (
+                (1 - segment.relative_age()) / FADE_OUT)
+        else:
+            return PLAYING_HEIGHT
 
     def segment_position(self, segment):
         x1 = self.byte_to_px(segment.begin)
