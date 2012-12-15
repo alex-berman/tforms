07208b2d0fe2128efb30c37c2d4ff587f74454a6
Alex Berman <alex@nimations.com>
2012-12-15 11:25:45 +0100

    press images white on black

--- make_press_images.sh
+++ make_press_images.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 ./plot_ancestry.py -width 2000 -height 4000 -stroke-width 6 sessions/*shadow/ -f -o press/torrential_forms1.svg
-convert press/torrential_forms1.svg press/torrential_forms1.tiff
+convert press/torrential_forms1.svg -negate press/torrential_forms1.jpg
 
 ./plot_ancestry.py -width 3000 -height 3000 -stroke-width 6 sessions/*too-big-darwin/ -f -o press/torrential_forms2.svg
-convert press/torrential_forms2.svg -rotate -90 press/torrential_forms2.tiff
+convert press/torrential_forms2.svg -negate -rotate -90 press/torrential_forms2.jpg
 
 ./plot_ancestry.py -width 4000 -height 4000 -stroke-width 6 sessions/*guthrie/ --geometry=circle -f -o press/torrential_forms3.svg
-convert press/torrential_forms3.svg press/torrential_forms3.tiff
+convert press/torrential_forms3.svg -negate press/torrential_forms3.jpg
+
+
 
 # ./plot_ancestry.py -width 4000 -height 4000 -stroke-width 6 sessions/*emma/ --geometry=circle -f -o press/torrential_forms3.svg
 # convert press/torrential_forms3.svg press/torrential_forms3.tiff
