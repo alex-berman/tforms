e677d1fbcd86c5915a51f849859be13d70558ebf
Alex Berman <alex@nimations.com>
2012-12-20 07:33:15 +0100

    world visible in 3d geography visualization

--- geo/visualize_geo.py
+++ geo/visualize_geo.py
@@ -2,25 +2,46 @@
 
 import world
 import sys
+from argparse import ArgumentParser
+
 sys.path.append("visual-experiments")
 import visualizer
 from OpenGL.GL import *
+from vector import Vector3d, Vector
 
-CAMERA_POSITION = Vector3d(-4.6, -0.6, -8.6)
-CAMERA_Y_ORIENTATION = -37
-CAMERA_X_ORIENTATION = 0
+CAMERA_POSITION = Vector(3, [-11.410326069762691, -7.499999999999989, -33.71008311478789])
+CAMERA_Y_ORIENTATION = 0
+CAMERA_X_ORIENTATION = 1
 
 class Geography(visualizer.Visualizer):
     def __init__(self, args):
         visualizer.Visualizer.__init__(self, args)
         self._set_camera_position(CAMERA_POSITION)
         self._set_camera_orientation(CAMERA_Y_ORIENTATION, CAMERA_X_ORIENTATION)
-        self._world = world.World(1.0, 1.0)
+        self._world = world.World(20.0, 20.0)
+        self.enable_accum()
+        self.enable_3d()
+
+    def InitGL(self):
+        visualizer.Visualizer.InitGL(self)
+        visualizer.NUM_ACCUM_SAMPLES = 1
+        #glClearColor(0.0, 0.0, 0.0, 0.0)
 
     def render(self):
-        self._render_world()
+        self.accum(self._render_world)
+        #self._render_world()
 
     def _render_world(self):
+        glColor3f(0,0,0)
+
+        for path in self._world.paths:
+            self._render_land(path)
+
+    def _render_land(self, path):
+        glBegin(GL_LINE_STRIP)
+        for x, y in path:
+            glVertex3f(x, 0, y)
+        glEnd()
 
 
 parser = ArgumentParser()
@@ -28,4 +49,4 @@ Geography.add_parser_arguments(parser)
 options = parser.parse_args()
 options.standalone = True
 
-Ancestry(options).run()
+Geography(options).run()
