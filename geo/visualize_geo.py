e840d68767b45e130bb9cfee46cdd5651804f407
Alex Berman <alex@nimations.com>
2012-12-21 14:40:26 +0100

    geo: parabolae from peers to berlin

--- geo/visualize_geo.py
+++ geo/visualize_geo.py
@@ -5,6 +5,7 @@ import sys
 from argparse import ArgumentParser
 import numpy
 import re
+import math
 
 import GeoIP
 import random
@@ -24,6 +25,9 @@ CAMERA_POSITION = Vector(3, [-17.237835534835536, -14.099999999999966, -24.48634
 CAMERA_Y_ORIENTATION = -1
 CAMERA_X_ORIENTATION = 38
 
+HERE_LATITUDE = 52.500556
+HERE_LONGITUDE = 13.398889
+
 WORLD_WIDTH = 30
 WORLD_HEIGHT = 20
 LOCATION_PRECISION = 200
@@ -32,6 +36,8 @@ class Geography(visualizer.Visualizer):
     def __init__(self, args):
         self._geo_ip = GeoIP.open("%s/GeoLiteCity.dat" % os.path.dirname(__file__), GeoIP.GEOIP_STANDARD)
         self._gps = GPS(WORLD_WIDTH, WORLD_HEIGHT)
+        self._here_x = self._gps.x(HERE_LONGITUDE)
+        self._here_y = self._gps.y(HERE_LATITUDE)
         self._load_locations()
         visualizer.Visualizer.__init__(self, args)
         self._set_camera_position(CAMERA_POSITION)
@@ -85,8 +91,10 @@ class Geography(visualizer.Visualizer):
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
         self._render_world()
-        self._render_bar_grid()
-        self._render_surface_points()
+        #self._render_bar_grid_lines()
+        #self._render_bar_grid_points()
+        self._render_parabolae()
+        self._render_land_points()
         #self._render_locations()
 
     def _render_world(self):
@@ -109,7 +117,7 @@ class Geography(visualizer.Visualizer):
             glVertex3f(x, BARS_TOP, y)
         glEnd()
 
-    def _render_bar_grid(self):
+    def _render_bar_grid_lines(self):
         glBegin(GL_LINES)
         ny = 0
         for row in self._grid:
@@ -132,7 +140,10 @@ class Geography(visualizer.Visualizer):
             ny += 1
         glEnd()
 
-    def _render_surface_points(self):
+    def _render_bar_grid_points(self):
+        self._render_grid_points(BARS_TOP)
+
+    def _render_grid_points(self, h):
         glEnable(GL_POINT_SMOOTH)
         glPointSize(3.0)
         glBegin(GL_POINTS)
@@ -143,12 +154,41 @@ class Geography(visualizer.Visualizer):
             nx = 0
             for value in row:
                 if value > 0:
-                    h = pow(float(value) / self._location_max_value, 0.2)
                     x = (nx+0.5) / LOCATION_PRECISION * WORLD_WIDTH
-                    glVertex3f(x, BARS_TOP, y)
+                    glVertex3f(x, h, y)
                 nx += 1
             ny += 1
         glEnd()
+
+    def _render_parabolae(self):
+        ny = 0
+        for row in self._grid:
+            y = (ny+0.5) / LOCATION_PRECISION * WORLD_HEIGHT
+            nx = 0
+            for value in row:
+                if value > 0:
+                    x = (nx+0.5) / LOCATION_PRECISION * WORLD_WIDTH
+                    strength = pow(float(value) / self._location_max_value, 0.4) * 0.8
+                    glColor4f(1, 1, 1, strength)
+                    self._render_parabola(x, y, self._here_x, self._here_y)
+                nx += 1
+            ny += 1
+
+    def _render_parabola(self, x1, y1, x2, y2):
+        h1 = 0
+        h2 = 2
+        glBegin(GL_LINE_STRIP)
+        precision = 100
+        for n in range(precision):
+            r = float(n) / (precision - 1)
+            x = x1 + (x2 - x1) * r
+            y = y1 + (y2 - y1) * r
+            h = h1 + (h2 - h1) * (math.cos((r - 0.5) / 5) - 0.995) / 0.005
+            glVertex3f(x, h, y)
+        glEnd()
+
+    def _render_land_points(self):
+        self._render_grid_points(0)
 
 parser = ArgumentParser()
 Geography.add_parser_arguments(parser)
