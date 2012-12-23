79a965d7d3c2593d998f50f8b22e7e5dfa948f89
Alex Berman <alex@nimations.com>
2012-12-23 14:35:52 +0100

    plot_ancestry.py: support for interpretation (optimization). outputs DOT-rendering command line.

--- plot_ancestry.py
+++ plot_ancestry.py
@@ -4,6 +4,7 @@ from tr_log_reader import *
 from argparse import ArgumentParser
 import ancestry_plotter
 import sys
+from interpret import Interpreter
 
 parser = ArgumentParser()
 parser.add_argument("sessiondir", type=str)
@@ -15,6 +16,7 @@ parser.add_argument("-height", type=int)
 parser.add_argument("-stroke-color", type=str, default="black")
 parser.add_argument("-f", "--force", action="store_true")
 parser.add_argument("-o", dest="output_filename")
+parser.add_argument("-interpret", action="store_true")
 ancestry_plotter.AncestryPlotter.add_parser_arguments(parser)
 args = parser.parse_args()
 
@@ -33,13 +35,23 @@ if args.selected_files:
 print >> sys.stderr, "found %d chunks" % len(log.chunks)
 log.ignore_non_downloaded_files()
 
+if args.interpret:
+    pieces = Interpreter().interpret(log.chunks)
+else:
+    pieces = log.chunks
+
 output = open(output_filename, "w")
 plotter_class = ancestry_plotter.OUTPUT_TYPES[args.output_type]
 plotter = plotter_class(log.total_file_size(), log.lastchunktime(), args)
 plotter.set_size(args.width, args.height)
-for chunk in log.chunks:
+for chunk in pieces:
     plotter.add_piece(chunk["id"], chunk["t"], chunk["begin"], chunk["end"])
 plotter.plot(output)
 
 output.close()
-print "plot: %s" % output_filename
+
+if args.output_type == "dot":
+    print "plot with:"
+    print "neato %s -Tps -o%s/ancestry_neato.ps" % (output_filename, args.sessiondir)
+else:
+    print "plot: %s" % output_filename
