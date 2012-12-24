660195fab687fb06489e7c8e1bec73cf4371f87d
Alex Berman <alex@nimations.com>
2012-12-24 13:42:41 +0100

    batch_plot_ancestry.py adapted for new tasks

--- batch_plot_ancestry.py
+++ batch_plot_ancestry.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python
 
 import session_library
-from ancestry_plotter import AncestryPlotter
+from ancestry_plotter import *
 import subprocess
 
 for session in session_library.get_sessions():
-    for geometry in AncestryPlotter.GEOMETRIES:
-        output_path = "graphs/ancestry/ancestry_%s_%s.svg" % (session["name"], geometry)
-        cmdline = "./plot_ancestry.py -width 2000 --geometry=%s -o %s %s" % (
+    if session["name"].startswith("A"):
+        geometry = CIRCLE
+        output_path = "graphs/ancestry_circle_straight_shrinking/ancestry_%s_%s.svg" % (session["name"], geometry)
+        cmdline = "./plot_ancestry.py -width 2000 -stroke-width 10 --geometry=%s -o %s %s --edge-style=line --stroke-style=shrinking" % (
             geometry, output_path, session["dir"])
         print cmdline
         subprocess.call(cmdline, shell=True)
+
+        # for geometry in AncestryPlotter.GEOMETRIES:
+        #     output_path = "graphs/ancestry/ancestry_%s_%s.svg" % (session["name"], geometry)
+        #     cmdline = "./plot_ancestry.py -width 2000 --geometry=%s -o %s %s" % (
+        #         geometry, output_path, session["dir"])
+        #     print cmdline
+        #     subprocess.call(cmdline, shell=True)
