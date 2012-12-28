e7520875a546d4427882c862ea2756cdb4521227
Alex Berman <alex@nimations.com>
2012-12-28 14:34:21 +0100

    updated demos/revisualize_ancestry_forward.sh

--- demos/revisualize_ancestry_forward.sh
+++ demos/revisualize_ancestry_forward.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./revisualize_ancestry.py --geometry=circle sessions/*chester -z 3 -width 1280 -height 720 --node-style=circle --unfold=forward -interpret --sway --edge-style=line --line-width=1 --node-size-envelope=0.5,25,0.5 --sway-envelope=0,50,0.5 --node-size=0.004
+./revisualize_ancestry.py --geometry=circle sessions/*chester -z 3 -width 1280 -height 720 --node-style=circle --unfold=forward -interpret --sway --edge-style=line --line-width=1 --node-size-envelope=0.5,10,0.5 --sway-envelope=0,50,0.5 --node-size=0.003
