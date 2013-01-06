8cf3abb147911ca07ebf351208e6f0397722d554
Alex Berman <alex@nimations.com>
2013-01-06 09:24:49 +0100

    fixed bug in geo/locations.py

--- geo/locations.py
+++ geo/locations.py
@@ -2,7 +2,7 @@ import cPickle
 import os
 
 def get_locations():
-    f = open("%s/scanner/locations.dat" % os.path.dirname(__file__), "r")
+    f = open("%s/scanner/locations.dat" % os.path.dirname(os.path.realpath(__file__)), "r")
     locations = cPickle.load(f)
     f.close()
     return locations
