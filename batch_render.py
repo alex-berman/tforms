ad2ad10062f6f1fc0074d2a688dbd06a4ac2d91c
Alex Berman <alex@nimations.com>
2013-01-07 07:12:03 +0100

    fixed bug in batch_render.py

--- batch_render.py
+++ batch_render.py
@@ -7,13 +7,15 @@ RESOLUTION = "-width 640 -height 360"
 FPS = 25
 
 sessions = [
-    ("*chopin", "-z 20"),
-    ("*adele", "-z 20"),
+#    ("*chopin", "-z 20"),
+#    ("*adele", "-z 20"),
+    ("*TDL4", ""),
     ]
 
 for (session, args) in sessions:
     cmd = "./revisualize_ancestry.py --geometry=circle sessions/%s %s %s --node-style=circle --unfold=forward -interpret --sway --edge-style=line --line-width=1 --node-size-envelope=0.5,10,0.5 --sway-envelope=0,50,0.5 --node-size=0.003 -export -export-fps=%s" % (session, args, RESOLUTION, FPS)
     subprocess.call(cmd, shell=True)
 
-    cmd = "./encode_exported.py -f -r %s sessions/%s Ancestry" % (FPS, session)
+    cmd = "./encode_exported.py -f -fps %s sessions/%s Ancestry" % (FPS, session)
+    print cmd
     subprocess.call(cmd, shell=True)
