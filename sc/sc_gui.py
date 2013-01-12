091e1fd06dcb99f4d96fed9ba0a62ef91268e707
Alex Berman <alex@nimations.com>
2013-01-12 09:35:51 +0100

    SC parameters are automatically saved to and loaded from disk

--- sc/sc_gui.py
+++ sc/sc_gui.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 
+CONFIG_FILENAME = "sc/parameters.config"
+
 import pygtk
 import gtk
+import cPickle
 
 import os
 import sys
-dirname = os.path.dirname(__file__)
-if dirname:
-    sys.path.append(dirname + "/..")
-else:
-    sys.path.append("..")
+sys.path.append(os.path.dirname(__file__) + "/..")
 from synth_controller import SynthController
 
 synth = SynthController()
@@ -17,11 +16,38 @@ synth = SynthController()
 parameters = ["mix", "room", "damp"]
 
 def value_changed(adj, parameter):
+    global values
     synth._send("/set_reverb_%s" % parameter, adj.value/100)
+    values[parameter] = adj.value/100
+    save_values()
 
 def destroy(widget, data=None):
     gtk.main_quit()
 
+def defalt_values():
+    values = {}
+    for parameter in parameters:
+        values[parameter] = 0.5
+    return values
+
+def load_values():
+    global values
+    try:
+        f = open(CONFIG_FILENAME, "rb")
+        values = cPickle.load(f)
+        f.close()
+    except (IOError, EOFError):
+        print "Warning: failed to load SC parameters from %s. Using default values." % CONFIG_FILENAME
+        values = defalt_values()
+
+def save_values():
+    global values
+    f = open(CONFIG_FILENAME, "wb")
+    cPickle.dump(values, f)
+    f.close()
+
+
+load_values()
 window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 window.set_default_size(200, 200)
 window.connect("destroy", destroy)
@@ -30,7 +56,7 @@ main_box = gtk.HBox(False, 0)
 
 for parameter in parameters:
     adj = gtk.Adjustment(
-        value=50.0, lower=0, upper=100,
+        value=values[parameter]*100, lower=0, upper=100,
         step_incr=1, page_incr=10, page_size=0)
 
     box = gtk.VBox(False, 0)
