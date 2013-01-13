2f79c648e8248b085c30fcdcf51c055efc11e1d7
Alex Berman <alex@nimations.com>
2013-01-13 14:35:46 +0100

    added titles to playlist

--- chb_playlist.py
+++ chb_playlist.py
@@ -1,34 +1,67 @@
 playlist = [
+    # VALIS
+    # gulliver
+    # dorian gray
+    # jane austen
+
     {"session": "sessions/*neuromancer-ch1-3",
-     "args": ["-z", "3", "--title=Neuromancer by William Gibson (chapters 1-3)"]},
+     "args": ["-z", "3",
+              "--title=Neuromancer by William Gibson (chapters 1-3)"]},
+
     {"session": "sessions/*brave-ch1-4",
-     "args": ["-z", "0.5"]},
+     "args": ["-z", "0.5",
+              "--title=Brave New World by Aldous Huxley (chapters 1-4)"]},
     {"session": "sessions/*brave-ch5-8",
-     "args": ["-z", "1"]},
+     "args": ["-z", "1",
+              "--title=Brave New World by Aldous Huxley (chapters 5-8)"]},
+
     {"session": "sessions/*1984-ch1-3",
-     "args": ["-z", "0.3"]},
+     "args": ["-z", "0.3",
+              "--title=1984 by George Orwell (chapters 1-3)"]},
+
     {"session": "sessions/*gatsby-ch1-5",
-     "args": ["-z", "0.5"]},
+     "args": ["-z", "0.5",
+              "--title=The Great Gatsby by F. Scott Fitzgerald (chapters 1-5)"]},
     {"session": "sessions/*gatsby-ch6-10",
-     "args": ["-z", "0.5"]},
+     "args": ["-z", "0.5",
+              "--title=The Great Gatsby by F. Scott Fitzgerald (chapters 6-10)"]},
     {"session": "sessions/*gatsby-ch11-15",
-     "args": ["-z", "0.5"]},
+     "args": ["-z", "0.5",
+              "--title=The Great Gatsby by F. Scott Fitzgerald (chapters 11-15)"]},
+
     {"session": "sessions/*art-of-war",
-     "args": ["-z", "1"]},
+     "args": ["-z", "1",
+              "--title=The Art of War by Sun Tzu"]},
+
     {"session": "sessions/*rings",
-     "args": ["-z", "0.5"]},
+     "args": ["-z", "0.5",
+              "--title=Lord of the Rings by J.R.R. Tolkien"]},
+
     {"session": "sessions/*sherlock",
-     "args": ["-z", "0.5"]},
+     "args": ["-z", "0.5",
+              "--title=The Return of Sherlock Holmes by Sir Arthur Conan Doyle"]},
+
     {"session": "sessions/*frankenstein",
-     "args": ["-z", "0.25"]},
+     "args": ["-z", "0.25",
+              "--title=Frankenstein by Mary Shelley"]},
+
     {"session": "sessions/*quran",
-     "args": ["-z", "0.25"]},
+     "args": ["-z", "0.25",
+              "--title=The Quran (Al-Fatiha and Al-Baqarah)"]},
+
     {"session": "sessions/*learn-in-car*",
-     "args": ["-z", "0.2"]},
+     "args": ["-z", "0.2",
+              "--title=Learn in your Car: German"]},
+
     {"session": "sessions/*hobbit*",
-     "args": ["-z", "0.25"]},
+     "args": ["-z", "0.25",
+              "--title=Hobbit by J.R.R Tolkien"]},
+
     {"session": "sessions/*hunger",
-     "args": ["-z", "8"]},
+     "args": ["-z", "8",
+              "--title=Catching Fire by Suzanne Collins"]},
+
     {"session": "sessions/*miracle",
-     "args": ["-z", "5"]},
+     "args": ["-z", "5",
+              "--title=The Miracle of Self-discipline by Tracy Brian"]},
 ]
