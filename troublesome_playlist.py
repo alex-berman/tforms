c3a01a97589b582a47fc93070745546afaed2bce
Alex Berman <alex@nimations.com>
2013-02-24 14:07:06 +0100

    added troublesome playlist, currently used for troubleshooting too many open files

---
+++ troublesome_playlist.py
@@ -0,0 +1,9 @@
+playlist = [
+    {"session": "sessions/*neuromancer-ch1-3",
+     "args": ["-z", "30",
+              "--title=Neuromancer by William Gibson (chapters 1-3)"]},
+
+    {"session": "sessions/*miracle",
+     "args": ["-z", "20",
+              "--title=The Miracle of Self-discipline by Tracy Brian"]},
+]
