22a69e90cdec0c7884031dc6b80ff053dcf34aa0
Alex Berman <alex@nimations.com>
2013-03-07 16:42:32 +0100

    first step towards magnet support

--- new_session.py
+++ new_session.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python
 
 from session import Session
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("-s", "session_name")
+parser.add_argument("-t", "torrent")
+args = parser.parse_args()
+
+# TODO:
+# mkdir ~/Downloads/$session_name
+# create configuration as copy of default config, with ~/Downloads/$session_name as download-dir
+# automatically add fileLocation=... to torrent init line in log
+# start transmission with configuration and torrent as arguments
+
 session = Session()
 session.start()
 session.join()
