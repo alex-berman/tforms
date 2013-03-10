8f2e75a8515586eb544e80fb8684b99f1a7d67ac
Alex Berman <alex@nimations.com>
2013-03-10 12:15:58 +0100

    added textival playlist

---
+++ textival_playlist.py
@@ -0,0 +1,11 @@
+playlist = [
+    {"session": "sessions/*zlatan-01-10",
+     "args": ["--pretend-audio=textival/kallocain1.wav",
+              "-z", "0.1"]},
+
+    # {"session": "sessions/*moberg-utvandrarna-01",
+    #  "args": ["-z", "0.5"]},
+
+    # {"session": "sessions/*astrid-lindgrens-favoriter-bocker",
+    #  "args": ["-z", "1"]},
+]
