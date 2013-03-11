7513be0c63c50df159118da2e2d4a0b4a0c0a54b
Alex Berman <alex@nimations.com>
2013-03-11 12:06:47 +0100

    updated playlist

--- textival_playlist.py
+++ textival_playlist.py
@@ -1,12 +1,12 @@
 playlist = [
     {"session": "sessions/*zlatan-01-10",
      "args": ["--pretend-audio=textival/kallocain1.wav",
-              "--title=Karin Boye: Kallocain, del 1",
+              "--title=Karin Boye: Kallocain (del 1)",
               "-z", "0.1"]},
 
     {"session": "sessions/*roda_rummet1",
      "args": ["--pretend-audio=textival/strindberg_brott_och_brott1.wav",
-              "--title=August Strindberg: Brott och brott, del 1",
+              "--title=August Strindberg: Brott och brott (del 1 av 6)",
               "-z", "1"]},
 
     # {"session": "sessions/*moberg-utvandrarna-01",
