alex-berman/tforms

commit 33f6e70c4dad864c2799f95a578aa798c717bfea

solved problem with caused visualizer to fade out before playback was finished

orchestra.py
@@ -106,6 +106,7 @@ class Server(OscReceiver):
         self.add_method("/place_segment", "ifff", self._handle, "_handle_place_segment")
         self.add_method("/enable_smooth_movement", "", self._handle, "_handle_enable_smooth_movement")
         self.add_method("/start_segment_movement_from_peer", "if", self._handle, "_handle_start_segment_movement_from_peer")
+        self.add_method("/finished", "", self._handle, "_handle_finished")
         self.start()
         self._num_registered_visualizers = 0
         server_thread = threading.Thread(name="%s.server_thread" % self.__class__.__name__,
@@ -487,12 +488,15 @@ class Orchestra:
 
     def play_non_realtime(self, quit_on_end=False):
         logger.info("entering play_non_realtime")
+        self._num_finished_visualizers = 0
         if self._loop:
             while True:
                 self._play_until_end()
+                self._wait_for_visualizers_to_finish()
                 self.set_time_cursor(0)
         else:
             self._play_until_end()
+            self._wait_for_visualizers_to_finish()
             if quit_on_end:
                 self._quitting = True
         logger.info("leaving play_non_realtime")
@@ -841,6 +845,13 @@ class Orchestra:
                 "length": os.stat(self.options.pretend_audio_filename).st_size,
                 "name": self.options.pretend_audio_filename,
                 "playable_file_index": 0}
+
+    def _handle_finished(self, path, args, types, src, data):
+        self._num_finished_visualizers += 1
+
+    def _wait_for_visualizers_to_finish(self):
+        while self._num_finished_visualizers < len(self.visualizers):
+            time.sleep(0.1)
         
 def warn(logger, message):
     logger.info(message)

orchestra_controller.py
@@ -25,3 +25,6 @@ class OrchestraController(simple_osc_sender.OscSender):
 
     def start_segment_movement_from_peer(self, segment_id, duration):
         self.send("/start_segment_movement_from_peer", segment_id, duration)
+
+    def notify_finished(self):
+        self.send("/finished")

visual-experiments/visualizer.py
@@ -290,6 +290,7 @@ class Visualizer:
         self.torrent_download_completion_time = None
         self.num_segments = 0
         self.num_received_segments = 0
+        self._notified_finished = False
 
     def enable_3d(self):
         self._3d_enabled = True
@@ -545,12 +546,16 @@ class Visualizer:
 
         glutSwapBuffers()
         self.previous_frame_time = self.now
-        if (self.export or self.args.exit_when_finished) and self.finished():
+        finished = self.finished()
+        if (self.export or self.args.exit_when_finished) and finished:
             self.exiting = True
         if self.export:
             self.exporter.export_frame()
 
         self._frame_count += 1
+        if finished and not self._notified_finished:
+            self.orchestra.notify_finished()
+            self._notified_finished = True
 
     def finished(self):
         return False
@@ -792,9 +797,12 @@ class Visualizer:
         if self.torrent_download_completion_time:
             return True
         else:
-            if self.num_segments > 0 and self.num_received_segments == self.num_segments:
+            if self.num_segments > 0 and self.num_received_segments == self.num_segments and not self.active():
                 self.torrent_download_completion_time = self.current_time()
                 return True
+
+    def active(self):
+        return False
 
     @staticmethod
     def add_parser_arguments(parser):

visual-experiments/waves.py
@@ -113,6 +113,9 @@ class Waves(visualizer.Visualizer):
         else:
             self.gathered_color = GATHERED_COLOR
 
+    def active(self):
+        return len(self.playing_segments) > 0
+
     def finished(self):
         if self.download_completed():
             time_after_completion = max(self.now - self.torrent_download_completion_time, 0)

commit 7bed7cbd8f16e33428882dee21f22ced0a3c7f67

fixed --preview bug introduced in 33f6e70c4dad864c2799f95a578aa798c717bfea

orchestra.py
@@ -311,6 +311,7 @@ class Orchestra:
         self.segments_by_id = {}
         self._playing = False
         self._quitting = False
+        self._was_stopped = False
         self.space = Space()
 
         self._scheduler_queue = Queue.Queue()
@@ -488,15 +489,18 @@ class Orchestra:
 
     def play_non_realtime(self, quit_on_end=False):
         logger.info("entering play_non_realtime")
+        self._was_stopped = False
         self._num_finished_visualizers = 0
         if self._loop:
             while True:
                 self._play_until_end()
-                self._wait_for_visualizers_to_finish()
+                if not self._was_stopped:
+                    self._wait_for_visualizers_to_finish()
                 self.set_time_cursor(0)
         else:
             self._play_until_end()
-            self._wait_for_visualizers_to_finish()
+            if not self._was_stopped:
+                self._wait_for_visualizers_to_finish()
             if quit_on_end:
                 self._quitting = True
         logger.info("leaving play_non_realtime")
@@ -564,6 +568,7 @@ class Orchestra:
         # stop_all disabled as it also deletes ~reverb
         # if self.synth:
         #     self.synth.stop_all()
+        self._was_stopped = True
         self._playing = False
         self.log_time_played_from = self.get_current_log_time()
         self.stopwatch.stop()