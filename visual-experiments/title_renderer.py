c9bdc1530001cfc39174068682f08ea7ec35e622
Alex Berman <alex@nimations.com>
2013-03-17 13:56:08 +0100

    refactoring

--- visual-experiments/title_renderer.py
+++ visual-experiments/title_renderer.py
@@ -1,16 +1,9 @@
 class TitleRenderer:
     def __init__(self, title, size, visualizer):
-        print "TitleRenderer"
         if ":" in title:
             author, book = title.split(":")
             title = "%s: %s" % (author.upper(), book)
-        self.title = title
-        self.size = size
-        self.visualizer = visualizer
+        self._text_renderer = visualizer.text_renderer(title, size)
 
     def render(self, x, y):
-        self.visualizer.draw_text(
-            text = self.title,
-            x = x,
-            y = y,
-            size = self.size)
+        self._text_renderer.render(x, y)
