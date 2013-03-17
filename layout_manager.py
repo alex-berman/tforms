dc229ce60ab4822aea9a1fe2b6f3be0da711b81e
Alex Berman <alex@nimations.com>
2013-03-17 07:40:45 +0100

    refactoring

---
+++ layout_manager.py
@@ -0,0 +1,22 @@
+class LayoutManager1d:
+    def __init__(self):
+        self._components = {}
+        self._id_count = 0
+
+    def add(self, begin, end):
+        for component in self._components.values():
+            if self._overlap(component["begin"], component["end"], begin, end):
+                return False
+        layout_component = self._id_count
+        self._components[layout_component] = {"begin": begin, "end": end}
+        self._id_count += 1
+        return layout_component
+
+    def _overlap(self, begin1, end1, begin2, end2):
+        return ((begin2 <= begin1 <= end2) or
+                (begin2 <= end1 <= end2) or
+                (begin1 <= begin2 <= end1) or
+                (begin1 <= end2 <= end1))
+
+    def remove(self, layout_component):
+        del self._components[layout_component]
--- visual-experiments/waves.py
+++ visual-experiments/waves.py
@@ -7,6 +7,7 @@ from vector import Vector3d
 from gatherer import Gatherer
 from math_tools import sigmoid
 import random
+from layout_manager import LayoutManager1d
 
 WAVEFORM_SIZE = 60
 WAVEFORM_MAGNITUDE = 30.0 / 480
@@ -328,29 +329,6 @@ class Waves(visualizer.Visualizer):
 
     def handle_segment_waveform_value(self, segment, value):
         segment.append_to_waveform(value)
-
-class LayoutManager1d:
-    def __init__(self):
-        self._components = {}
-        self._id_count = 0
-
-    def add(self, begin, end):
-        for component in self._components.values():
-            if self._overlap(component["begin"], component["end"], begin, end):
-                return False
-        layout_component = self._id_count
-        self._components[layout_component] = {"begin": begin, "end": end}
-        self._id_count += 1
-        return layout_component
-
-    def _overlap(self, begin1, end1, begin2, end2):
-        return ((begin2 <= begin1 <= end2) or
-                (begin2 <= end1 <= end2) or
-                (begin1 <= begin2 <= end1) or
-                (begin1 <= end2 <= end1))
-
-    def remove(self, layout_component):
-        del self._components[layout_component]
 
 if __name__ == "__main__":
     parser = ArgumentParser()
