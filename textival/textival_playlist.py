5ef53c8305630a7972863da846a90f64b91498f3
Alex Berman <alex@nimations.com>
2013-03-18 13:38:56 +0100

    extended playlist

--- textival/textival_playlist.py
+++ textival/textival_playlist.py
@@ -9,8 +9,16 @@ playlist = [
               "--title=August Strindberg: Brott och brott (del 1 av 6)",
               "-z", "1"]},
 
-    # {"session": "sessions/*moberg-utvandrarna-01",
-    #  "args": ["-z", "0.5"]},
+    {"session": "sessions/*moberg-utvandrarna-01",
+     "args": ["--pretend-audio=textival/moberg_utvandrarna1_1.wav",
+              "--title=Vilhelm Moberg: Utvandrarna (del 1 av 8)",
+              "-z", "0.6"]},
+
+    {"session": "sessions/*moberg-utvandrarna-02",
+     "args": ["--pretend-audio=textival/moberg_utvandrarna1_2.wav",
+              "--title=Vilhelm Moberg: Utvandrarna (del 2 av 8)",
+              "--max-segment-duration=5.0", "--max-pause-within-segment=2.5",
+              "-z", "2.5"]},
 
     # {"session": "sessions/*astrid-lindgrens-favoriter-bocker",
     #  "args": ["-z", "1"]},
