e66d8905e9f614752637d8d15ec1d8618c723d6a
Alex Berman <alex@nimations.com>
2013-03-21 12:20:00 +0100

    extended playlist

--- textival/textival_playlist.py
+++ textival/textival_playlist.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 playlist = [
     {"session": "sessions/*zlatan-01-10",
      "args": ["--pretend-audio=textival/audio/kallocain1.wav",
@@ -6,34 +8,40 @@ playlist = [
 
     {"session": "sessions/*roda_rummet1",
      "args": ["--pretend-audio=textival/audio/strindberg_brott_och_brott1.wav",
-              "--title=August Strindberg: Brott och brott (del 1 av 6)",
+              "--title=August Strindberg: Brott och brott (del 1)",
               "-z", "1"]},
 
     {"session": "sessions/*moberg-utvandrarna-01",
      "args": ["--pretend-audio=textival/audio/moberg_utvandrarna1_1.wav",
-              "--title=Vilhelm Moberg: Utvandrarna (del 1 av 8)",
+              "--title=Vilhelm Moberg: Utvandrarna (del 1)",
               "-z", "0.6"]},
 
     {"session": "sessions/*moberg-utvandrarna-02",
      "args": ["--pretend-audio=textival/audio/moberg_utvandrarna1_2.wav",
-              "--title=Vilhelm Moberg: Utvandrarna (del 2 av 8)",
+              "--title=Vilhelm Moberg: Utvandrarna (del 2)",
               "--max-segment-duration=5.0", "--max-pause-within-segment=2.5",
               "-z", "2.5"]},
 
     {"session": "sessions/*roda_rummet2",
      "args": ["--pretend-audio=textival/audio/ibsen_ett_dockhem1.wav",
-              "--title=Henrik Ibsen: Ett dockhem (del 1 av 6)",
+              "--title=Henrik Ibsen: Ett dockhem (del 1)",
               "-z", "4",
               "--max-segment-duration=5.0", "--max-pause-within-segment=2.5"]},
 
     {"session": "sessions/*astrid-lindgrens-favoriter-bocker",
      "args": ["--pretend-audio=textival/audio/flygare_carlen_nyckfull1.wav",
-              "--title=Emilie Flygare-Carl\\xe9n: En nyckfull kvinna (del 1 av 2)",
+              u"--title=Emilie Flygare-Carlén: En nyckfull kvinna (del 1)".encode("unicode_escape"),
               "-z", "0.6"]},
 
     {"session": "sessions/*zlatan-11-20",
      "args": ["--pretend-audio=textival/audio/lagerloef_nils1.wav",
-              "--title=Selma Lagerl\xf6f: Nils Holgersson's Adventures (part 1)",
+              u"--title=Selma Lagerlöf: Nils Holgersson's Adventures (part 1)".encode("unicode_escape"),
               "-z", "0.7",
+              "--max-segment-duration=5.0", "--max-pause-within-segment=2.5"]},
+
+    {"session": "sessions/*roda_rummet3",
+     "args": ["--pretend-audio=textival/audio/dagerman_sommar1.wav",
+              u"--title=Stig Dagerman: Vår lilla sommar (del 1)".encode("unicode_escape"),
+              "-z", "4.5",
               "--max-segment-duration=5.0", "--max-pause-within-segment=2.5"]},
 ]
