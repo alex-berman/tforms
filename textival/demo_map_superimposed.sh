0616689e24487b798d6287feaba2b2a46a863f2b
Alex Berman <alex@nimations.com>
2013-03-21 08:54:16 +0100

    restructured folders

---
+++ textival/demo_map_superimposed.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./play.py --locate-peers --visualizer="python visual-experiments/waves_and_heat_map.py -width 800 -height 640 -left 0 -top 0 --waves-margin=0.01,0,0.065,0 --text-renderer=ftgl --font=fonts/simplex_.ttf" sessions/*roda_rummet1 --pretend-audio=textival/audio/strindberg_brott_och_brott1.wav -z 1 --sc-mode=rear_to_front_4ch_reverb --title="Karin Boye: Kallocain (del 1)"
--- textival/textival_playlist.py
+++ textival/textival_playlist.py
@@ -1,27 +1,27 @@
 playlist = [
     {"session": "sessions/*zlatan-01-10",
-     "args": ["--pretend-audio=textival/kallocain1.wav",
+     "args": ["--pretend-audio=textival/audio/kallocain1.wav",
               "--title=Karin Boye: Kallocain (del 1)",
               "-z", "0.1"]},
 
     {"session": "sessions/*roda_rummet1",
-     "args": ["--pretend-audio=textival/strindberg_brott_och_brott1.wav",
+     "args": ["--pretend-audio=textival/audio/strindberg_brott_och_brott1.wav",
               "--title=August Strindberg: Brott och brott (del 1 av 6)",
               "-z", "1"]},
 
     {"session": "sessions/*moberg-utvandrarna-01",
-     "args": ["--pretend-audio=textival/moberg_utvandrarna1_1.wav",
+     "args": ["--pretend-audio=textival/audio/moberg_utvandrarna1_1.wav",
               "--title=Vilhelm Moberg: Utvandrarna (del 1 av 8)",
               "-z", "0.6"]},
 
     {"session": "sessions/*moberg-utvandrarna-02",
-     "args": ["--pretend-audio=textival/moberg_utvandrarna1_2.wav",
+     "args": ["--pretend-audio=textival/audio/moberg_utvandrarna1_2.wav",
               "--title=Vilhelm Moberg: Utvandrarna (del 2 av 8)",
               "--max-segment-duration=5.0", "--max-pause-within-segment=2.5",
               "-z", "2.5"]},
 
     {"session": "sessions/*roda_rummet2",
-     "args": ["--pretend-audio=textival/ibsen_ett_dockhem1.wav",
+     "args": ["--pretend-audio=textival/audio/ibsen_ett_dockhem1.wav",
               "--title=Henrik Ibsen: Ett dockhem (del 1 av 6)",
               "-z", "4",
               "--max-segment-duration=5.0", "--max-pause-within-segment=2.5"]}
