da47a4c59269f085bf7955e95eaa66abc1307f94
Alex Berman <alex@nimations.com>
2013-03-22 08:56:53 +0100

    updated playlist

--- textival/run.sh
+++ textival/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./sequencer.py --locate-peers --visualizer="python visual-experiments/waves_and_heat_map.py -width 800 -height 640 -left 0 -top 0 --map-margin=0.7,0,0,0 --waves-margin=0,0,0.3,0 --text-renderer=ftgl --font=fonts/simplex_.ttf --peer-info" --playlist="textival.textival_playlist" --sc-mode=rear_to_front_4ch_reverb
+./sequencer.py --locate-peers --visualizer="python visual-experiments/waves_and_heat_map.py -width 1024 -height 576 -left 0 -top 0 --map-margin=0.7,0,0,0 --waves-margin=0,0,0.3,0 --text-renderer=ftgl --font=fonts/simplex_.ttf --peer-info" --playlist="textival.textival_playlist" --sc-mode=rear_to_front_4ch_reverb
--- textival/textival_playlist.py
+++ textival/textival_playlist.py
@@ -6,6 +6,12 @@ playlist = [
               "--title=Karin Boye: Kallocain (del 1)",
               "-z", "0.1"]},
 
+    {"session": "sessions/*zlatan-11-20",
+     "args": ["--pretend-audio=textival/audio/kallocain2.wav",
+              "--title=Karin Boye: Kallocain (del 2)",
+              "-z", "0.5",
+              "--max-segment-duration=5.0", "--max-pause-within-segment=2.5"]},
+
     {"session": "sessions/*roda_rummet1",
      "args": ["--pretend-audio=textival/audio/strindberg_brott_och_brott1.wav",
               "--title=August Strindberg: Brott och brott (del 1)",
@@ -33,15 +39,18 @@ playlist = [
               u"--title=Emilie Flygare-Carlén: En nyckfull kvinna (del 1)".encode("unicode_escape"),
               "-z", "0.6"]},
 
-    {"session": "sessions/*zlatan-11-20",
-     "args": ["--pretend-audio=textival/audio/lagerloef_nils1.wav",
-              u"--title=Selma Lagerlöf: Nils Holgersson's Adventures (part 1)".encode("unicode_escape"),
-              "-z", "0.7",
-              "--max-segment-duration=5.0", "--max-pause-within-segment=2.5"]},
-
     {"session": "sessions/*roda_rummet3",
      "args": ["--pretend-audio=textival/audio/dagerman_sommar1.wav",
               u"--title=Stig Dagerman: Vår lilla sommar (del 1)".encode("unicode_escape"),
               "-z", "4.5",
               "--max-segment-duration=5.0", "--max-pause-within-segment=2.5"]},
+
+
+
+    # REMOVED:
+    # {"session": "sessions/*zlatan-11-20",
+    #  "args": ["--pretend-audio=textival/audio/lagerloef_nils1.wav",
+    #           u"--title=Selma Lagerlöf: Nils Holgersson's Adventures (part 1)".encode("unicode_escape"),
+    #           "-z", "0.7",
+    #           "--max-segment-duration=5.0", "--max-pause-within-segment=2.5"]},
 ]
