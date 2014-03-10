a1037fc4a07239de47ee7722034eee108d594fe5
Alex Berman <alex@nimations.com>
2014-03-10 14:56:09 +0100

    support for concatenating revisualized ancestry animations into one video file

--- batch_render_revisualized_ancestry.py
+++ batch_render_revisualized_ancestry.py
@@ -11,72 +11,68 @@ FPS = 25
 # RESOLUTION = "-width 640 -height 360"
 # FPS = 30
 
-sessions_grouped_by_geometry = {
-    "circle": [
-        ("*roda_rummet2", "-z 60"),
-        ("*utvandrarna-02", "-z 60"),
-        ("*NIN", "-z 60"),
-
-        ("*valis", "-z 25"),
-        ("*lord-rings", "-z 15"),
-        ("*miracle", "-z 20"),
-        ("*gulliver", "-z 4"),
-        ("*potter", "-z 30"),
-        ("*frankenstein", "-z 6"),
-        ("*learn*", "-z 2"),
-        ("*chopin", "-z 20"),
-        ("*adele", "-z 20"),
-
-        ("*guthrie", "-z 20"),
-        ("*hunger", "-z 60"),
-        ("*arvo-part", "-z 30"),
-        ],
-
-    "rect": [
-        ("*roda_rummet2", "-z 60"),
-        ("*utvandrarna-02", "-z 60"),
-        ("*NIN", "-z 60"),
-
-        ("*miracle", "-z 20"),
-        ("*gulliver", "-z 4"),
-        ("*potter", "-z 30"),
-        ("*frankenstein", "-z 6"),
-        ("*chopin", "-z 20"),
-
-        ("*guthrie", "-z 20"),
-        ("*hunger", "-z 60"),
-        ("*arvo-part", "-z 30"),
-        ],
-    }
+sessions = [
+    ("circle", "*roda_rummet2", "-z 60"),
+    ("rect",   "*arvo-part", "-z 30"),
+    ("circle", "*utvandrarna-02", "-z 60"),
+    ("rect",   "*gulliver", "-z 4"),
+    ("circle", "*NIN", "-z 60"),
+    ("rect",   "*guthrie", "-z 20"),
+    ("circle", "*valis", "-z 25"),
+    ("rect",   "*hunger", "-z 60"),
+    ("circle", "*lord-rings", "-z 15"),
+    ("circle", "*miracle", "-z 20"),
+    ("rect",   "*roda_rummet2", "-z 60"),
+    ("circle", "*potter", "-z 30"),
+    ("circle", "*frankenstein", "-z 6"),
+    ("rect",   "*potter", "-z 30"),
+    ("circle", "*learn*", "-z 2"),
+    ("rect",   "*NIN", "-z 60"),
+    ("circle", "*gulliver", "-z 4"),
+    ("rect",   "*miracle", "-z 20"),
+    ("circle", "*adele", "-z 20"),
+    ("rect",   "*utvandrarna-02", "-z 60"),
+    ("circle", "*guthrie", "-z 20"),
+    ("rect",   "*frankenstein", "-z 6"),
+    ("circle", "*hunger", "-z 60"),
+    ("circle", "*arvo-part", "-z 30"),
+    ]
 
 parser = ArgumentParser()
 parser.add_argument("--force", "-f", action="store_true")
+parser.add_argument("--concat")
 args = parser.parse_args()
 
-for geometry, sessions in sessions_grouped_by_geometry.iteritems():
-    for (session_pattern, render_args) in sessions:
-        matches = glob.glob("sessions/%s" % session_pattern)
-        if len(matches) == 1:
-            session_dir = matches[0]
-        elif len(matches) == 0:
-            raise Exception("no sessions matching %s" % session_pattern)
-        else:
-            raise Exception("more than one session matching %s" % session_pattern)
+video_filenames = []
+for geometry, session_pattern, render_args in sessions:
+    matches = glob.glob("sessions/%s" % session_pattern)
+    if len(matches) == 1:
+        session_dir = matches[0]
+    elif len(matches) == 0:
+        raise Exception("no sessions matching %s" % session_pattern)
+    else:
+        raise Exception("more than one session matching %s" % session_pattern)
 
-        render_dir = "%s/rendered_Ancestry_%s" % (session_dir, geometry)
-        print "rendering into %s" % render_dir
-        if os.path.exists(render_dir) and not args.force:
-            print "rendering directory exists - skipping"
-        else:
-            cmd = "./revisualize_ancestry.py --geometry=%s %s %s %s --node-style=circle -interpret --sway --sway-magnitude=0.003 --edge-style=line --line-width=1 --node-size-envelope=0.3,25,0.5,0.2 --root-node-size-envelope=0.1,0,1,0.2 --sway-envelope=0,50,0.5,1 --node-size=0.003 --root-node-size=0.005 --prune-out -export -export-fps=%s -export-dir=%s" % (geometry, session_dir, render_args, RESOLUTION, FPS, render_dir)
-            subprocess.call(cmd, shell=True)
+    render_dir = "%s/rendered_Ancestry_%s" % (session_dir, geometry)
+    print "rendering into %s" % render_dir
+    if os.path.exists(render_dir) and not args.force:
+        print "rendering directory exists - skipping"
+    else:
+        cmd = "./revisualize_ancestry.py --geometry=%s %s %s %s --node-style=circle -interpret --sway --sway-magnitude=0.003 --edge-style=line --line-width=1 --node-size-envelope=0.3,25,0.5,0.2 --root-node-size-envelope=0.1,0,1,0.2 --sway-envelope=0,50,0.5,1 --node-size=0.003 --root-node-size=0.005 --prune-out -export -export-fps=%s -export-dir=%s" % (geometry, session_dir, render_args, RESOLUTION, FPS, render_dir)
+        subprocess.call(cmd, shell=True)
 
-        video_filename = "rendered_ancestry/%s_ancestry_%s.mp4" % (
-            os.path.basename(session_dir), geometry)
-        print "encoding into %s" % video_filename
-        if os.path.exists(video_filename) and not args.force:
-            print "video file exists - skipping"
-        else:
-            cmd = "./encode_exported.py -f -fps %s %s -o %s" % (
-                FPS, render_dir, video_filename)
-            subprocess.call(cmd, shell=True)
+    video_filename = "rendered_ancestry/%s_ancestry_%s.mp4" % (
+        os.path.basename(session_dir), geometry)
+    print "encoding into %s" % video_filename
+    video_filenames.append(video_filename)
+    if os.path.exists(video_filename) and not args.force:
+        print "video file exists - skipping"
+    else:
+        cmd = "./encode_exported.py -f -fps %s %s -o %s" % (
+            FPS, render_dir, video_filename)
+        subprocess.call(cmd, shell=True)
+
+if args.concat:
+    cmd = "mencoder -oac pcm -ovc copy -o %s %s" % (
+        args.concat, " ".join(video_filenames))
+    subprocess.call(cmd, shell=True)
