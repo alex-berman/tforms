60a694a5348b170ac7bb8de1583fbdf0e7568383
Alex Berman <alex@nimations.com>
2014-03-10 11:38:19 +0100

    revisualized ancestry: updated selection and slightly modified timing

--- batch_render_revisualized_ancestry.py
+++ batch_render_revisualized_ancestry.py
@@ -13,29 +13,39 @@ FPS = 25
 
 sessions_grouped_by_geometry = {
     "circle": [
-        # ("*valis", "-z 15", [1, 2]),
-        # ("*ulysses", "-z 100", [1]),
-        # ("*lord-rings", "-z 10", [1, 2]),
-        # ("*miracle", "-z 10", [1, 2]),
-        # ("*gulliver", "-z 2", [1, 2]),
-        # ("*potter", "-z 5", [2]),
-        # ("*frankenstein", "-z 5", [1, 2]),
-        # ("*learn*", "", [1, 2]),
-        # ("*hunger", "-z 40", [1, 2]),
-        # ("*chopin", "-z 20", [1, 2]),
-        # ("*adele", "-z 20", [1]),
+        ("*roda_rummet2", "-z 60"),
+        ("*utvandrarna-02", "-z 60"),
+        ("*NIN", "-z 60"),
 
-        # ("*guthrie", "-z 20", [1]),
-        ("*hunger", "-z 40", [1]),
-        # ("*arvo-part", "-z 30", [1]),
-        # ("*utvandrarna-02", "-z 60", [1]),
-        # ("*roda_rummet2", "-z 60", [1]),
+        ("*valis", "-z 25"),
+        ("*lord-rings", "-z 15"),
+        ("*miracle", "-z 20"),
+        ("*gulliver", "-z 4"),
+        ("*potter", "-z 30"),
+        ("*frankenstein", "-z 6"),
+        ("*learn*", "-z 2"),
+        ("*chopin", "-z 20"),
+        ("*adele", "-z 20"),
+
+        ("*guthrie", "-z 20"),
+        ("*hunger", "-z 60"),
+        ("*arvo-part", "-z 30"),
         ],
 
     "rect": [
-        ("*roda_rummet2", "-z 60", [1]),
-        # ("*utvandrarna-02", "-z 60", [1]),
-        # ("*NIN", "-z 60", [1]),
+        ("*roda_rummet2", "-z 60"),
+        ("*utvandrarna-02", "-z 60"),
+        ("*NIN", "-z 60"),
+
+        ("*miracle", "-z 20"),
+        ("*gulliver", "-z 4"),
+        ("*potter", "-z 30"),
+        ("*frankenstein", "-z 6"),
+        ("*chopin", "-z 20"),
+
+        ("*guthrie", "-z 20"),
+        ("*hunger", "-z 60"),
+        ("*arvo-part", "-z 30"),
         ],
     }
 
@@ -44,7 +54,7 @@ parser.add_argument("--force", "-f", action="store_true")
 args = parser.parse_args()
 
 for geometry, sessions in sessions_grouped_by_geometry.iteritems():
-    for (session_pattern, render_args, timefactors) in sessions:
+    for (session_pattern, render_args) in sessions:
         matches = glob.glob("sessions/%s" % session_pattern)
         if len(matches) == 1:
             session_dir = matches[0]
@@ -61,13 +71,12 @@ for geometry, sessions in sessions_grouped_by_geometry.iteritems():
             cmd = "./revisualize_ancestry.py --geometry=%s %s %s %s --node-style=circle -interpret --sway --sway-magnitude=0.003 --edge-style=line --line-width=1 --node-size-envelope=0.3,25,0.5,0.2 --root-node-size-envelope=0.1,0,1,0.2 --sway-envelope=0,50,0.5,1 --node-size=0.003 --root-node-size=0.005 --prune-out -export -export-fps=%s -export-dir=%s" % (geometry, session_dir, render_args, RESOLUTION, FPS, render_dir)
             subprocess.call(cmd, shell=True)
 
-        for timefactor in timefactors:
-            video_filename = "rendered_ancestry/%s_ancestry_z%s_%s.mp4" % (
-                os.path.basename(session_dir), timefactor, geometry)
-            print "encoding into %s" % video_filename
-            if os.path.exists(video_filename) and not args.force:
-                print "video file exists - skipping"
-            else:
-                cmd = "./encode_exported.py -f -fps %s %s -o %s" % (
-                    FPS * timefactor, render_dir, video_filename)
-                subprocess.call(cmd, shell=True)
+        video_filename = "rendered_ancestry/%s_ancestry_%s.mp4" % (
+            os.path.basename(session_dir), geometry)
+        print "encoding into %s" % video_filename
+        if os.path.exists(video_filename) and not args.force:
+            print "video file exists - skipping"
+        else:
+            cmd = "./encode_exported.py -f -fps %s %s -o %s" % (
+                FPS, render_dir, video_filename)
+            subprocess.call(cmd, shell=True)
